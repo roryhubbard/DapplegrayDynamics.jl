b566a86632410eca39cdc215d523880cd427f02c
Rory Hubbard <calvin3h@gmail.com>
2025-05-31 22:58:20 -0700

    touchup LQRCost

--- src/objective.jl
+++ src/objective.jl
@@ -1,16 +1,28 @@
-struct LQRCost <: SingleKnotPointFunction
-    Q::AbstractMatrix
-    R::AbstractMatrix
-    xd::AbstractVector
-    ud::AbstractVector
+struct LQRCost{T} <: SingleKnotPointFunction
+    Q::AbstractMatrix{T}
+    R::AbstractMatrix{T}
+    xd::AbstractVector{T}
+    ud::AbstractVector{T}
     idx::UnitRange{Int}
     nknots::Int
     outputdim::Int
+
+    function LQRCost(Q::AbstractMatrix{T}, R::AbstractMatrix{T},
+                     xd::AbstractVector{T}, ud::AbstractVector{T},
+                     idx::UnitRange{Int}) where {T}
+        @assert size(Q, 1) == size(Q, 2) == length(xd) "Q and xd must be consistent"
+        @assert size(R, 1) == size(R, 2) == length(ud) "R and ud must be consistent"
+        return new{T}(Q, R, xd, ud, idx, 1, 1)
+    end
+
+    function LQRCost(Q::AbstractMatrix{T}, R::AbstractMatrix{T},
+                     xd::AbstractVector{T}, idx::UnitRange{Int}) where {T}
+        @assert size(Q, 1) == size(Q, 2) == length(xd) "Q and xd must be consistent"
+        ud = zeros(T, size(R, 2))
+        return new{T}(Q, R, xd, ud, idx, 1, 1)
+    end
 end
-function LQRCost(Q::AbstractMatrix, R::AbstractMatrix, xd::AbstractVector, idx::UnitRange{Int})
-    ud = zeros(size(R, 2))
-    LQRCost(Q, R, xd, ud, idx, 1, 1)
-end
+
 function (cost::LQRCost)(z::AbstractVector, nstates::Int)
     x = @view z[1:nstates]
     u = @view z[nstates+1:end]
