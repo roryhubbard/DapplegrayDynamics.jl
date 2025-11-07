cc7bea7a8150d49c17f244a560efa04d0e96bcd4
Rory Hubbard <calvin3h@gmail.com>
2025-11-06 23:00:06 -0800

    fix import

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using DapplegrayDynamics
 using Test
 
 @testset "DapplegrayDynamics.jl" begin
-    acrobot = DD.load_acrobot()
+    acrobot = DapplegrayDynamics.load_acrobot()
     acrobot_swingup(acrobot, 50, 10.0)
-    pendulum = DD.load_pendulum()
+    pendulum = DapplegrayDynamics.load_pendulum()
     pendulum_swingup(pendulum, 51, 10.0)
     pendulum_swingup_nlopt(pendulum, 51, 10.0, 10)
 end
