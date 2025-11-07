81746a1f16cde5b031d142bc469812cee9659c76
Rory Hubbard <calvin3h@gmail.com>
2025-11-06 22:53:10 -0800

    move plotting out

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,9 @@ using DapplegrayDynamics
 using Test
 
 @testset "DapplegrayDynamics.jl" begin
-    df()
-    kj()
-    nl()
+    acrobot = DD.load_acrobot()
+    acrobot_swingup(acrobot, 50, 10.0)
+    pendulum = DD.load_pendulum()
+    pendulum_swingup(pendulum, 51, 10.0)
+    pendulum_swingup_nlopt(pendulum, 51, 10.0, 10)
 end
